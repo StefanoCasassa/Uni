73c51879434266a23da37a0d91479643dd0a1b73
StefanoCasassa <165203966+StefanoCasassa@users.noreply.github.com>
2024-11-17 21:09:33 +0100

    Add files via upload

---
+++ Secondo anno/LPO/lab01-2024-lpo24-15/Lab01/Program.fs
@@ -0,0 +1,154 @@
+﻿//Es.1
+(*
+mulAll : int list -> int
+
+mulAll ls restituisce il prodotto di tutti i numeri interi contenuti nella lista ls.
+*)
+let rec mulAll ls = 
+    match ls with
+    | po::lo -> po * mulAll lo
+    | [] -> 1
+
+assert (mulAll (2 :: 4 :: 3 :: []) = 24)
+
+//Es.2
+(*
+isIn : 'a -> 'a list -> bool
+
+isIn el ls restituisce true se e solo se el è un elemento della lista ls.
+*)
+let rec isIn el ls = 
+    match ls with
+    | po::lo -> if po=el then true else isIn el lo
+    | [] -> false
+
+assert isIn 3 (2 :: 4 :: 3 :: [])
+assert not (isIn 5 (2 :: 4 :: 3 :: []))
+
+//Es.3
+(*insert : 'a -> 'a list -> 'a list
+
+insert el ls restituisce la lista ottenuta aggiungendo el in fondo alla lista ls se el non appartiene
+già a ls; restituisce ls altrimenti.
+
+Esempio:*)
+
+let rec insert  el ls =
+    match ls with 
+    | po::lo -> if po=el then ls else  po::(insert el lo)
+    | [] -> ls@[el]
+
+assert (insert 0 (2 :: 4 :: 3 :: []) = 2 :: 4 :: 3 :: 0 :: [])
+assert (insert 3 (2 :: 4 :: 3 :: []) = 2 :: 4 :: 3 :: [])
+//Es.4
+(*insert2 : 'a -> 'a list -> 'a list
+
+come l'esercizio precedente, ma provare a usare una funzione ricorsiva ausiliaria per evitare di passare
+a ogni chiamata ricorsiva l'elemento da inserire.
+*)
+
+
+let insert2 (el: 'a) (ls: 'a list)=
+    let rec aux (ls: 'a list) : 'a list= 
+        match ls with
+        | hd::tl -> if hd = el then ls else hd::aux tl
+        | [] -> [el]
+    aux ls
+    
+    
+
+
+assert (insert2 0 (2 :: 4 :: 3 :: []) = 2 :: 4 :: 3 :: 0 :: [])
+assert (insert2 3 (2 :: 4 :: 3 :: []) = 2 :: 4 :: 3 :: [])
+//Es.5
+(*
+`odd : 'a list -> 'a list` 
+    
+`odd ls` restituisce la lista ottenuta da `ls` tenendo solo gli elementi di indice dispari, considerando
+che gli indici iniziano da 0.
+*)
+
+let odd ls =
+    let rec aux index ls =
+        match ls with
+        | [] -> []
+        | hd :: tl -> if index % 2 <> 0 then hd :: aux (index + 1) tl else aux (index + 1) tl
+    aux 0 ls
+
+
+assert (odd (7 :: 3 :: 4 :: 1 :: 2 :: 5 :: []) = 3 :: 1 :: 5 :: [])
+
+//Es.6
+(*
+`ordInsert : 'a -> 'a list -> 'a list` 
+
+assumendo che `ls` sia già ordinata in modo crescente e senza ripetizioni, `ordInsert el ls` restituisce
+la lista ordinata in modo crescente e senza ripetizioni ottenuta aggiungendo `el` a `ls`.
+*)
+let rec ordInsert el ls =
+    match ls with
+    | [] -> ls@[el]
+    | hd::tl -> if hd >= el then (if hd = el then ls else el::hd::tl) else hd :: (ordInsert el tl)
+
+assert (ordInsert 0 (1 :: 2 :: 4 :: 5 :: []) = 0 :: 1 :: 2 :: 4 :: 5 :: [])
+assert (ordInsert 3 (1 :: 2 :: 4 :: 5 :: []) = 1 :: 2 :: 3 :: 4 :: 5 :: [])
+assert (ordInsert 7 (1 :: 2 :: 4 :: 5 :: []) = 1 :: 2 :: 4 :: 5 :: 7 :: [])
+assert (ordInsert 2 (1 :: 2 :: 4 :: 5 :: []) = 1 :: 2 :: 4 :: 5 :: [])
+
+//Es.7
+(*
+`ordInsert2 : 'a -> 'a list -> 'a list` 
+come l'esercizio precedente, ma provare a usare una funzione ricorsiva ausiliaria per evitare di passare 
+a ogni chiamata ricorsiva l'elemento da inserire.
+*)
+let  ordInsert2 el ls =
+    let rec aux1 ls=
+        match ls with
+        | [] -> ls@[el]
+        | hd::tl -> if hd >= el then (if hd = el then ls else el::hd::tl) else hd :: (aux1 tl)
+    aux1 ls
+
+assert (ordInsert2 0 (1 :: 2 :: 4 :: 5 :: []) = 0 :: 1 :: 2 :: 4 :: 5 :: [])
+assert (ordInsert2 3 (1 :: 2 :: 4 :: 5 :: []) = 1 :: 2 :: 3 :: 4 :: 5 :: [])
+assert (ordInsert2 7 (1 :: 2 :: 4 :: 5 :: []) = 1 :: 2 :: 4 :: 5 :: 7 :: [])
+assert (ordInsert2 2 (1 :: 2 :: 4 :: 5 :: []) = 1 :: 2 :: 4 :: 5 :: [])
+//Es.8
+(*
+`merge : 'a list * 'a list -> 'a list` 
+    
+assumendo che `ls1` e `ls2` siano già ordinate in modo crescente e senza ripetizioni, `merge (ls1,ls2)` 
+restituisce la lista ordinata in modo crescente e senza ripetizioni ottenuta fondendo assieme `ls1` e `ls2`. 
+
+    
+*Nota*: F# permette di abbreviare le due keyword `else` `if` con la singola `elif`. 
+*)
+
+let rec merge (ls1,ls2) =
+    match ls1,ls2 with 
+    | [],[] -> []
+    | [],_ -> ls2
+    | _,[] -> ls1
+    | ab::lu,bb::ly -> if ab<bb then ab::merge (lu,ls2) else bb::merge (ls1,ly)
+
+
+assert (merge ((1 :: 3 :: 5 :: []), (2 :: 4 :: 6 :: [])) = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: [])
+assert (merge ((1 :: 2 :: 3 :: []), (4 :: 5 :: [])) = 1 :: 2 :: 3 :: 4 :: 5 :: [])
+assert (merge ((3 :: []), (1 :: 2 :: 4 :: 5 :: [])) = 1 :: 2 :: 3 :: 4 :: 5 :: [])
+//Es.9
+(*
+`curriedMerge : 'a list -> 'a list -> 'a list` a
+    
+definire `curriedMerge`, la versione curried di `merge`, direttamente senza usare `merge`.
+*)
+
+let rec curriedMerge ls1 ls2 =
+    match ls1,ls2 with 
+    | [],[] -> []
+    | [],_ -> ls2
+    | _,[] -> ls1
+    | ab::lu,bb::ly -> if ab<bb then ab::curriedMerge lu ls2 else bb::curriedMerge ls1 ly
+
+
+assert (curriedMerge (1 :: 3 :: 5 :: []) (2 :: 4 :: 6 :: []) = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: [])
+assert (curriedMerge (1 :: 2 :: 3 :: []) (4 :: 5 :: []) = 1 :: 2 :: 3 :: 4 :: 5 :: [])
+assert (curriedMerge (3 :: []) (1 :: 2 :: 4 :: 5 :: []) = 1 :: 2 :: 3 :: 4 :: 5 :: [])
---
+++ Secondo anno/LPO/lab02-2024-lpo24-15/LAB2/Program.fs
@@ -0,0 +1,157 @@
+﻿
+(*Esercizi proposti
+Definire catAll : string list -> string tale che catAll l restituisce la stringa ottenuta concatenando tutte le stringhe di l secondo l'ordine determinato dalla lista. Utilizzare un parametro di accumulazione per ottenere ricorsione di coda.
+
+Esempio:
+*)
+
+let catAll (s:string list) :string  =
+    let rec loop (acc:string) (s:string list) =
+        match s with 
+        | lo::t1 -> loop (acc + lo ) t1 
+        | [] -> acc
+    loop "" s
+
+assert (catAll ("This" :: " is " :: "awesome!"::[]) = "This is awesome!")
+
+(*Definire catAllFold : string list -> string, equivalente a catAll, ma ottenuta come specializzazione di List.fold.*)
+let catAll2 (s:string list) =
+    List.fold (fun (acc:string) (k:string) -> acc+k) "" s
+
+assert (catAll2 ("This" :: " is " :: "awesome!"::[]) = "This is awesome!")
+(*
+Definire zip : 'a list -> 'b list -> ('a * 'b) list tale che
+
+zip (a_0::...::a_n::[]) (b_0::...::b_n::[]) = (a_0,b_0)::...::(a_n,b_n)::[]
+La funzione solleva l'eccezione Failure "different length" se le due liste hanno lunghezza diversa. Utilizzare un parametro di accumulazione per ottenere ricorsione di coda.
+*)
+
+let failwith msg = 
+    raise (Failure "different length")
+
+let rec zip a b  =
+    match a,b with
+    |[],[] -> []
+    |[],_ -> failwith "different length"
+    |_,[] -> failwith "different length"
+    |p1::t1,p2::t2 -> (p1,p2) :: zip t1 t2 
+
+
+assert (zip [] [] = [])
+ 
+assert (zip (1 :: 2 :: 3 :: []) ("a" :: "b" :: "c" :: []) = (1, "a") :: (2, "b") :: (3, "c") :: [])
+ 
+assert
+     ((try
+         zip (1 :: 2 :: []) ("a" :: "b" :: "c" :: [])
+       with
+       | Failure "different length" -> []) = [])
+
+(*
+Definire minEl : 'a list -> 'a option tale che minEl restituisce None se la lista è vuota, altrimenti Some m, con m il minimo della lista; usare la funzione predefinita min : 'a -> 'a -> 'a per semplificare il codice. Utilizzare un parametro di accumulazione per ottenere ricorsione di coda.
+
+Esempi:
+*)
+let minEl (a:'a list) : 'a option =
+    if a=[] then None else Some (List.min a) 
+
+assert (minEl [] = None)
+
+assert (minEl (3 :: 4 :: 6 :: -1 :: []) = Some -1)
+
+assert (minEl ("orange" :: "apple" :: "banana" :: []) = Some "apple")
+
+(*Definire minElFold : 'a list -> 'a option, equivalente a minEl, ma ottenuta usando List.fold.*)
+
+let minElFold (a:'a list) =
+    if a=[] then None else
+    let b =List.fold (fun acc k -> if k < acc then k else acc) (List.head a) (List.tail a)
+    Some b 
+
+assert (minElFold [] = None)
+
+assert (minElFold (3 :: 4 :: 6 :: -1 :: []) = Some -1)
+
+assert (minElFold ("orange" :: "apple" :: "banana" :: []) = Some "apple")
+
+type Direction =
+    |North 
+    |South
+    |East 
+    |West 
+
+let versor c  =
+    match c with
+    | North -> (0,1)
+    | East -> (1,0)
+    | South -> (0,-1)
+    | West -> (-1,0)
+
+assert (versor North = (0,1))
+assert (versor East = (1,0))
+assert (versor South = (0,-1))
+assert (versor West = (-1,0))
+
+type action =
+    | Turn of Direction
+    | Step of int
+
+(*
+Definire move : direction * (int * int) -> action -> direction * (int * int) che, presa una coppia, che definisce la direzione e le coordinate cartesiane iniziali,
+ e presa un'azione, restituisce la coppia costituita dalla direzione e coordinate cartesiane finali ottenute compiendo l'azione.
+*)
+
+let move ((a:Direction), ((b:int),(c:int))) (d:action) =
+    match d with
+    |Turn South -> (South,(b,c))
+    |Turn North -> (North,(b,c))
+    |Turn East -> (East,(b,c))
+    |Turn West -> (West,(b,c))
+    |Step k-> if a=South then (a,(b,c-k)) elif a=North then (a,(b,c+k)) elif a=East then (a,(b+k,c)) else (a,(b-k,c))
+
+
+assert (move (North, (0, 0)) (Turn South) = (South, (0, 0)))
+assert (move (North, (0, 0)) (Step 2) = (North, (0, 2)))
+assert (move (North, (0, 0)) (Step -1) = (North, (0, -1)))
+assert (move (North, (0, 0)) (Step 0) = (North, (0, 0)))
+
+
+(*Usando List.fold, definire la funzione doAll : direction * (int * int) -> action list -> direction * (int * int) che, 
+presa una coppia, che definisce la direzione e le coordinate cartesiane iniziali, e presa una lista di azioni, 
+restituisce la coppia costituita dalla direzione e coordinate cartesiane finali ottenute compiendo tutte le azioni della lista nell'ordine.
+
+Esempio:*)
+let doAll   ((a:Direction), ((b:int),(c:int))) (d:action list) = 
+    List.fold (fun ((a:Direction), ((b:int),(c:int))) (e:action) -> move (a,(b,c)) e ) (a,(b,c)) d
+
+assert
+(doAll
+    (North, (0, 0))
+    (Step 2
+     :: Turn East
+        :: Step 2
+           :: Step -1 :: Turn South :: Step 3 :: Step 0 :: []) = (South, (1, -1)))
+
+
+(*(Difficile) Dichiarare il tipo discriminated union polimorfo 'a tree degli alberi con nodi etichettati con valori di tipo 'a e numero arbitrario di figli.
+
+Definire la funzione postOrder : 'a tree -> 'a list che restituisce la lista delle etichette dell'albero ottenuta tramite visita post-order 
+(prima i nodi figlio da sinistra a destra, poi il nodo radice).
+
+Suggerimento: usare un parametro di accumulazione e List.fold. La definizione ricorsiva è di coda?
+
+Esempio:
+*)
+
+(*
+Da fare
+*)
+
+assert
+   (Node(
+      1,
+      (Node(2, [])
+         :: Node(3, [])
+            :: Node(4, (Node(5, []) :: Node(6, []) :: [])) :: [])
+   ))
+   |> postOrder = (2 :: 3 :: 5 :: 6 :: 4 :: 1 :: [])    
